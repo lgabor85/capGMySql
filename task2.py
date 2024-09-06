81dd4102d14e57cbfab7b248f127c4b25851a9f3
lgabor85 <laagabinl@gmail.com>
2024-09-06 19:06:23 +0200

    Add employee details to employee_detail.txt

---
+++ task2.py
@@ -0,0 +1,29 @@
+import os
+import mysql.connector
+from tabulate import tabulate
+
+# Step 1: Read the environment variables
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
+# Step 2: Establish a connection to the MySQL database
+conn = mysql.connector.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
+)
+
+# Write an SQL query to fetch unique values of “city” from “customers” table using the alias name as “CUSTOMER_CITY” and output the result in a tabular format into a file named “unique_cities.txt”.
+file = open('employee_detail.txt', 'w')
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM employees WHERE firstName NOT IN ('Gerard', 'Mary')")
+result = cursor.fetchall()
+file.write(tabulate(result, headers=['employeeNumber', 'lastName', 'firstName', 'extension', 'email', 'officeCode', 'reportsTo', 'jobTitle'], tablefmt='grid'))
+file.close()
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
