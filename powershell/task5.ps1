9ed336f6ec8b20d3c355d667097dd049c3c4e81c
lgabor85 <laagabinl@gmail.com>
2024-09-09 09:45:47 +0200

    Add PowerShell scripts for MySQL data queries

---
+++ powershell/task5.ps1
@@ -0,0 +1,28 @@
+# Load the MySQLCmdlets module
+Import-Module MySQLCmdlets
+
+# Read the environment variables
+$db_host = $env:DB_HOST
+$db_user = $env:DB_USER
+$db_password = $env:DB_PASSWORD
+$db_name = $env:DB_NAME
+$db_port = $env:DB_PORT
+
+# Establish a connection to the MySQL database
+$conn = Connect-MySQL -Server $db_host -Database $db_name -User $db_user -Password $db_password -Port $db_port
+
+# Write an SQL query to fetch unique values of “city” from “customers” table using the alias name as “CUSTOMER_CITY” and output the result in a tabular format into a file named “unique_cities.txt”.
+$query = "SELECT o.territory, COUNT(e.employeeNumber) AS employee_count FROM employees e JOIN offices o ON e.officeCode = o.officeCode GROUP BY o.territory ORDER BY employee_count DESC;"
+
+# Execute the query and fetch the results
+$result = Invoke-MySQL -Connection $conn -Query $query
+
+# Convert the results to a table format
+$results = $result | Format-Table -AutoSize | Out-String
+
+# Write the results to a file
+$outfile = "emea_employee_count.txt"
+$results | Out-File -FilePath $outfile
+
+# Close the connection
+Disconnect-MySQL -Connection $conn
---
+++ powershell/task6.ps1
@@ -0,0 +1,31 @@
+# Load the MySQLCmdlets module
+Import-Module MySQLCmdlets
+
+# Read the environment variables
+$db_host = $env:DB_HOST
+$db_user = $env:DB_USER
+$db_password = $env:DB_PASSWORD
+$db_name = $env:DB_NAME
+$db_port = $env:DB_PORT
+
+# Establish a connection to the MySQL database
+$conn = Connect-MySQL -Server $db_host -Database $db_name -User $db_user -Password $db_password -Port $db_port
+
+# Write an SQL query to fetch unique values of “city” from “customers” table using the alias name as “CUSTOMER_CITY” and output the result in a tabular format into a file named “unique_cities.txt”.
+$query = "SELECT p.productName, pl.textDescription 
+    AS productLine FROM products p 
+    JOIN productlines pl ON p.productLine = pl.productLine 
+    ORDER BY pl.textDescription ASC, p.productName DESC;"
+
+# Execute the query and fetch the results
+$result = Invoke-MySQL -Connection $conn -Query $query
+
+# Convert the results to a table format
+$results = $result | Format-Table -AutoSize | Out-String
+
+# Write the results to a file
+$outfile = "unique_product.txt"
+$results | Out-File -FilePath $outfile
+
+# Close the connection
+Disconnect-MySQL -Connection $conn
